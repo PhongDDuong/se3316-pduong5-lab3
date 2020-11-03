4091a4f54685aed072ae204ead6f0a2671a38d67
Phong Duong <pduong5@uwo.ca>
2020-11-03 18:34:34 -0500

    create and delete schedules

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const { response } = require('express');
 const express = require('express');
 var router = express.Router();
 const data = require("./Lab3-timetable-data.json");
+const Storage = require('node-storage');
 const Joi = require('joi');
 const app = express();
 
 app.use(express.json());
+
+var store = new Storage('schedule');
+
+const asdasd = {
+  id: "asd", 
+  name: "asddasd"
+}
+store.put('banana', asdasd);
+store.put('hello', "asdasdasdasddas");
 
 const port = process.env.Port || 3000;
 
@@ -23,12 +32,62 @@ app.use(function (req, res, next) {
   next();
 })
 
+//get all schedules
+app.get('/api/schedule', (req, res) => {
+  const result = [];
+
+  for(schedule in store.store) {
+    result.push(schedule);
+  }
+  res.send(result);
+});
+
+
+app.get('/api/schedule/:id', (req, res) => {
+  res.send(store.get(req.params.id));
+});
+
+//put schedules
+app.put('/api/schedule/:id', (req, res) => {
+  store.put(req.params.id,"");
+
+  var newData = JSON.stringify(store.store)
+  const schedules = [JSON.parse(newData)];
+  res.send(schedules);
+});
+
+app.put('/api/schedule/:id/:id2', (req, res) => {
+  store.put(req.params.id,req.params.id2);
+
+  var newData = JSON.stringify(store.store)
+  const schedules = [JSON.parse(newData)];
+  res.send(schedules);
+});
+
+app.delete('/api/schedule/', function (req, res) {
+
+  for(schedule in store.store) {
+    store.remove(schedule);
+  }
+
+  res.send("deleted");
+});
+
+app.delete('/api/schedule/:id', function (req, res) {
+
+  store.remove(req.params.id);
+
+  res.send("deleted");
+});
+
+
+
 //get all courses
 router.get('/', (req, res) => {
   res.send(courses);
 });
 
-//add new part
+//add new course
 router.post('/', function (req, res) {
   const { error } = validateCourse(req.body); //result.error
 
@@ -102,37 +161,3 @@ app.use('/api/courses', router);
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
-
-//get all subject codes and descriptions
-/*router.get('/', function (req, res) {
-  const result = [];
-  for(course of courses){
-    result.push(course.subject,course.className);
-  }
-  res.send(result);
-})*/
-//get course using subject code
-/*router.get('/:id', function (req, res) {
-  //const course = courses.find(c => c.subject === req.params.class_nbr);
-  //if(!course) return res.status(404).send('Course not found');
-  const result = [];
-  for(course of courses){
-    if(course.subject==req.params.id){
-      result.push(course.catalog_nbr);
-    }
-  }
-  //courses.filter(course => course.subject.indexOf(req.params.id) !== -1);
-  res.send(result);
-});*/
-
-/*router.get('/:id/:id2', function (req, res) {
-  //const course = courses.find(c => c.subject === req.params.class_nbr);
-  //if(!course) return res.status(404).send('Course not found');
-  const result = [];
-  for(course of courses){
-    if(course.subject==req.params.id && course.catalog_nbr==req.params.id2){
-      result.push(course.course_info.start_time);
-    }
-  }
-  res.send(result);
-});*/
--- static/schedule.js
+++ static/schedule.js
@@ -1,10 +1,17 @@
 document.getElementById('search-courses').addEventListener('click', courseSearch);
-document.getElementById('add-courses').addEventListener('click', addCourse);
+document.getElementById('schedule-create').addEventListener('click', makeSchedule);
+document.getElementById('schedules').addEventListener('onchange', displaySchedule);
+document.getElementById('schedule-display').addEventListener('click', displaySchedule);
+document.getElementById('schedule-delete').addEventListener('click', deleteSchedule);
+document.getElementById('schedule-delete-all').addEventListener('click', deleteAllSchedules);
+document.getElementById('add-course').addEventListener('click', addCourse);
+
+
 getCourses();
+getSchedule();
 
 function getCourses(){
-    var node= document.getElementById("courses");
-    node.querySelectorAll('*').forEach(n => n.remove());
+    clearArea("courses");
 
     fetch("/api/courses")
     .then(res => res.json()
@@ -34,11 +41,119 @@ function getCourses(){
     )
 }
 
+function getSchedule(){
+
+    clearArea("schedules");
+
+    fetch('/api/schedule').then(res => res.json())
+    .then(data => {
+        const l = document.getElementById('schedules');
+        var empty = true;
+
+        data.forEach(e => {
+            const option = document.createElement('option');
+            option.appendChild(document.createTextNode(e));
+            option.value = e;
+            l.appendChild(option);
+            empty = false;
+            
+        })
+        if(empty){
+            const l = document.getElementById('schedules');
+            const option = document.createElement('option');
+            option.appendChild(document.createTextNode("No Schedules"));
+            option.value ="No Schedules";
+            l.appendChild(option)
+        }
+        
+    })
+}
+
+function makeSchedule(){
+    const l = document.getElementById('schedule-name').value;
+    fetch('/api/schedule/'+ l, {
+        method: 'PUT',
+    })
+    .then(res => console.log(res))
+    getSchedule();
+}
+
+function displaySchedule(){
+    clearArea('schedule-area');
+    
+    const l = document.getElementById('schedules').value;
+    fetch('/api/schedule/'+ l, {
+        method: 'GET',
+    })
+    .then(res =>  res.json())
+    .then(data => {
+        const l = document.getElementById('schedule-area');
+        const option = document.createElement('p');
+        option.appendChild(document.createTextNode(data));
+        l.appendChild(option);
+        newCourses.push(data);
+    });
+}
+
+
+function deleteSchedule(){
+    const l = document.getElementById('schedules');
+    console.log(l.value)
+    fetch('/api/schedule/'+ l.value, {
+        method: 'DELETE',
+    })
+    .then(res => console.log(res))
+    getSchedule();
+}
+
+function deleteAllSchedules(){
+    const l = document.getElementById('schedules');
+    console.log(l.value)
+    fetch('/api/schedule/' , {
+        method: 'DELETE',
+    })
+    .then(res => console.log(res))
+    getSchedule();
+}
+
+function addCourse(){
+    /*
+    fetch("/api/courses")
+    .then(res => res.json()
+    .then(data => {
+        const l = document.getElementById("schedule-area");
+        if(document.getElementById('courseSubject').value!=="" && document.getElementById('courseCatalog_nbr').value!==""){
+            data.forEach(e => {
+                if(document.getElementById('courseSubject').value==e.subject && document.getElementById('courseCatalog_nbr').value==e.catalog_nbr){
+                    newCourses.push(e.subject,e.catalog_nbr);
+                    updateSchedule(newCourses);
+
+                }
+            })
+        }
+        else{
+            const error = document.createElement('li');
+            error.appendChild(document.createTextNode("No courses found"))
+            l.appendChild(error);
+        }
+    })
+    )*/
+    console.log("pls")
+    displaySchedule();
+}
+
+
+
+function clearArea(area){
+    const node= document.getElementById(area);
+    node.querySelectorAll('*').forEach(n => n.remove());
+}
+
 function courseSearch(){
+    getSchedule()
     var found = false;
 
-    var node= document.getElementById("courses");
-    node.querySelectorAll('*').forEach(n => n.remove())
+    clearArea("courses");
 
     fetch("/api/courses")
     .then(res => res.json()
@@ -59,7 +174,7 @@ function courseSearch(){
             data.forEach(e => {
                 if(document.getElementById('subject').value==e.subject && document.getElementById('catalog_nbr').value==e.catalog_nbr){
                     const course = document.createElement('li');
-                    course.appendChild(document.createTextNode(`${e.course_info[0].start_time}, ${e.course_info[0].end_time}`))
+                    course.appendChild(document.createTextNode(`${e.course_info[0].days}, ${e.course_info[0].start_time}, ${e.course_info[0].end_time}`))
                     l.appendChild(course);
                     found = true;
                 }
@@ -69,7 +184,7 @@ function courseSearch(){
             data.forEach(e => {
                 if(document.getElementById('subject').value==e.subject && document.getElementById('catalog_nbr').value==e.catalog_nbr && document.getElementById('ssr_component').value==e.course_info[0].ssr_component){
                     const course = document.createElement('li');
-                    course.appendChild(document.createTextNode(`${e.course_info[0].start_time}, ${e.course_info[0].end_time}`))
+                    course.appendChild(document.createTextNode(`${e.course_info[0].days}, ${e.course_info[0].start_time}, ${e.course_info[0].end_time}`))
                     l.appendChild(course);
                     found = true;
                 }
@@ -82,36 +197,4 @@ function courseSearch(){
         }
     })
     )
-}
-
-function addCourse(){
-    const course = {
-        //id: document.getElementById('id').value,
-        name: document.getElementById('name').value
-    }
-    console.log(course);
-
-    fetch('/api/courses', {
-        method: 'POST',
-        headers: {'Content-type': 'application/json'},
-        body: JSON.stringify(course)
-    })
-    .then(res =>{
-        if(res.ok) {
-            res.json()
-            .then(data => {
-                console.log(data);
-                getCourses();
-                document.getElementById('status').innerText = `Created course ${data.id}: ${data.name}`;
-            })
-                
-
-            .catch(err => console.log("failure"))
-        }
-        else{
-            console.log('Error: ', res.status)
-            document.getElementById('status').innerText = 'Failed to add item';
-        }
-    })
-    .catch()
 }
