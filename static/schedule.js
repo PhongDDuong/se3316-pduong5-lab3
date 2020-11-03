ccfd588ac271bb03efd6c0e32ee633333bb778d0
Phong Duong <pduong5@uwo.ca>
2020-11-03 12:35:10 -0500

    improved search and returns error if no courses are found

--- static/schedule.js
+++ static/schedule.js
@@ -1,25 +1,42 @@
-document.getElementById('get-courses').addEventListener('click', courseSearch);
+document.getElementById('search-courses').addEventListener('click', courseSearch);
 document.getElementById('add-courses').addEventListener('click', addCourse);
 getCourses();
 
 function getCourses(){
     var node= document.getElementById("courses");
-    node.querySelectorAll('*').forEach(n => n.remove())
+    node.querySelectorAll('*').forEach(n => n.remove());
+
     fetch("/api/courses")
     .then(res => res.json()
     .then(data => {
         console.log(data);
         const l = document.getElementById('courses');
         data.forEach(e => {
-            const course = document.createElement('li');
-            course.appendChild(document.createTextNode(`${e.subject}, ${e.className}`))
-            l.appendChild(course);
+            const table = document.createElement('table');
+            const row = document.createElement('tr');
+            const cell = document.createElement('td');
+
+            cell.appendChild(document.createTextNode(`${e.subject}`))
+
+            row.appendChild(cell)
+            const cell2 = document.createElement('td');
+
+            cell2.appendChild(document.createTextNode(`${e.className}`))
+
+            row.appendChild(cell2)
+            table.appendChild(row)
+            l.appendChild(table);
+
+            //const br = document.createElement('br');
+            //l.appendChild(br);
         })
     })
     )
 }
 
 function courseSearch(){
+    var found = false;
+
     var node= document.getElementById("courses");
     node.querySelectorAll('*').forEach(n => n.remove())
 
@@ -28,17 +45,44 @@ function courseSearch(){
     .then(data => {
         console.log(data);
         const l = document.getElementById('courses');
-        data.forEach(e => {
-            if(document.getElementById('id').value==e.subject){
-                const course = document.createElement('li');
-                course.appendChild(document.createTextNode(`${e.catalog_nbr}`))
-                l.appendChild(course);
-            }
-        })
+        if(document.getElementById('subject').value!=="" && document.getElementById('catalog_nbr').value=="" && document.getElementById('ssr_component').value==""){
+            data.forEach(e => {
+                if(document.getElementById('subject').value==e.subject){
+                    const course = document.createElement('li');
+                    course.appendChild(document.createTextNode(`${e.catalog_nbr}`))
+                    l.appendChild(course);
+                    found = true;
+                }
+            })
+        }
+        if(document.getElementById('subject').value!=="" && document.getElementById('catalog_nbr').value!=="" && document.getElementById('ssr_component').value==""){
+            data.forEach(e => {
+                if(document.getElementById('subject').value==e.subject && document.getElementById('catalog_nbr').value==e.catalog_nbr){
+                    const course = document.createElement('li');
+                    course.appendChild(document.createTextNode(`${e.course_info[0].start_time}, ${e.course_info[0].end_time}`))
+                    l.appendChild(course);
+                    found = true;
+                }
+            })
+        }
+        if(document.getElementById('subject').value!=="" && document.getElementById('catalog_nbr').value!=="" && document.getElementById('ssr_component').value!==""){
+            data.forEach(e => {
+                if(document.getElementById('subject').value==e.subject && document.getElementById('catalog_nbr').value==e.catalog_nbr && document.getElementById('ssr_component').value==e.course_info[0].ssr_component){
+                    const course = document.createElement('li');
+                    course.appendChild(document.createTextNode(`${e.course_info[0].start_time}, ${e.course_info[0].end_time}`))
+                    l.appendChild(course);
+                    found = true;
+                }
+            })
+        }
+        if(found == false){
+            const error = document.createElement('li');
+            error.appendChild(document.createTextNode("No courses found"))
+            l.appendChild(error);
+        }
     })
     )
 }
-
 
 function addCourse(){
     const course = {
